8b14aa88e6f246120c219336f001a057a38006a8
HeathTaurant <ksp1324@gmail.com>
2022-01-18 23:31:39 +0900

    wow

---
+++ Converter/src/CSVReader.java
@@ -0,0 +1,9 @@
+import java.io.File;
+
+public class CSVReader {
+    public static void main(String[] args) {
+        File csv = new File("C:/Users/lhh13/OneDrive/바탕 화면/programing/SPKim/whereisit/Converter/src/resources/csv/Dongjak-gu_Seoul_clothes collection box_20210902.csv");
+        
+    }
+    
+}
