1351cd5225ba69fb172247e383a980858b670ef5
HeathTaurant <ksp1324@gmail.com>
2022-01-20 21:07:33 +0900

    CSV reader

--- Converter/src/CSVReader.java
+++ Converter/src/CSVReader.java
@@ -1,9 +1,47 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CSVReader {
     public static void main(String[] args) {
-        File csv = new File("C:/Users/lhh13/OneDrive/바탕 화면/programing/SPKim/whereisit/Converter/src/resources/csv/Dongjak-gu_Seoul_clothes collection box_20210902.csv");
-        
+        CSVReader csvReader = new CSVReader();
+        csvReader.readCSV();
     }
+
+    public List<List<String>> readCSV() {
+        List<List<String>> csvList = new ArrayList<List<String>>();
+        File csv = new File("C:/Users/lhh13/OneDrive/바탕 화면/programing/SPKim/whereisit/Converter/src/resources/csv/Dongjak-gu_Seoul_clothes collection box_20210902.csv");
+        BufferedReader br = null;
+        String line = "";
+        try{
+            br = new BufferedReader(new FileReader(csv));
+            while ((line = br.readLine()) != null) { //readLine()은 파일에서 개행된 한 줄의 데이터를 읽어온다.
+                List<String> aLine = new ArrayList<String>();
+                String[] lineArr = line.split(","); //파일의 한 줄을 , 로 나누어 배열에 저장 후 리스트로 변환한다.
+                aLine = Arrays.asList(lineArr);
+                csvList.add(aLine);
+            }
+        }catch(FileNotFoundException e ){
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+            
+        }return csvList;
+    }
+
     
+
 }
